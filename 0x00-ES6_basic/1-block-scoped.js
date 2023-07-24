c395a632429aef4ff04ca502161e65f481d784d9
Johnfavour <favourjohn851@gmail.com>
2023-07-24 07:43:52 -0700

    modifying block scope

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,13 @@
+function taskBlock() {
+  let x = 10;
+  let y = 5;  
+
+  if (x > y) {
+    let z = x + y;
+    console.log("Inside the block - z:", z);
+  }
+  console.log("Outside the block - x:", x);
+  console.log("Outside the block - y:", y);
+}
+taskBlock();
+
