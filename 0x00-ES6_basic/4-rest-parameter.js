0491ef495637f1db548859bd52b48b9b5c1d6f57
Johnfavour <favourjohn851@gmail.com>
2023-07-24 08:37:36 -0700

    Applying the Rest parameter syntax for functions

--- 0x00-ES6_basic/4-rest-parameter.js
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -1,6 +1,3 @@
 export default function returnHowManyArguments(...args) {
   return args.length;
 }
-console.log(returnHowManyArguments());
-console.log(returnHowManyArguments(1, 2, 3)); 
-console.log(returnHowManyArguments('apple', 'banana', 'orange', 'grape'));
