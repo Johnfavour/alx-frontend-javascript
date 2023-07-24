f21dca052b7c1b0d1bf9ddd91e9551643d40fe55
Johnfavour <favourjohn851@gmail.com>
2023-07-24 07:13:15 -0700

    modifying const or let

---
+++ 0x00-ES6_basic/0-constants.js
@@ -0,0 +1,19 @@
+function taskFirst() {
+  const message = "This is a constant message.";
+  console.log(message);
+}
+
+function taskNext() {
+  let count = 0; 
+  console.log("Count:", count);
+
+  count++; 
+  console.log("Count after increment:", count);
+  
+  count = 10;
+  console.log("Count reassigned:", count);
+}
+
+taskFirst();
+taskNext();
+
