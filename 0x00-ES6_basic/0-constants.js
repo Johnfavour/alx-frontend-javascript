fbb27804fcdca2f095335d10355a2aa94a822b63
Johnfavour <favourjohn851@gmail.com>
2023-07-24 07:36:00 -0700

    modifying const or let

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,17 @@
-export function taskFirst() {
-  var task = 'I prefer const when I can.';
-  return task;
+function taskFirst() {
+  const message = "This is a constant message.";
+  console.log(message);
 }
+function taskNext() {
+  let count = 0;
+  console.log("Count:", count);
 
-export function getLast() {
-  return ' is okay';
+  count++; 
+  console.log("Count after increment:", count);
+  
+  count = 10;
+  console.log("Count reassigned:", count);
 }
+taskFirst();
+taskNext();
 
-export function taskNext() {
-  var combination = 'But sometimes let';
-  combination += getLast();
-
-  return combination;
-}
