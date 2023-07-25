24afd9556e50960505b3b7c504dd0a98a9d73bc6
Johnfavour <favourjohn851@gmail.com>
2023-07-25 21:12:11 +0000

    Returning a Promise using a function

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,25 +1,15 @@
-function YourConstructor() {
-  YourConstructor.prototype.getResponseFromAPI = function () {
-   
-    return new Promise((resolve, reject) => {
-      
-      fetch('https://api.example.com/data')
-        .then(response => {
-          if (!response.ok) {
-           
-            reject(new Error('Request failed with status ' + response.status));
-          } else {
-            
-            return response.json();
-          }
-        })
-        .then(data => {
-          resolve(data);
-        })
-        .catch(error => {
-   
-          reject(error);
-        });
-    });
-  };
+function getResponseFromAPI() {
+	  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+	          const responseData = { message: 'Hello from the API!' };
+	    resolve(responseData);
+	     }, 2000);
+  });
 }
+getResponseFromAPI()
+  .then(responseData => {
+	    console.log(responseData);
+  })
+  .catch(error => {
+	   console.error('Error:', error.message);
+  });
