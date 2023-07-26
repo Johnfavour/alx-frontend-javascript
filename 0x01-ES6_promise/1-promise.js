6293df963661b2056dd41de3623c3b3e2fb9df1b
Johnfavour <favourjohn851@gmail.com>
2023-07-26 18:51:29 +0000

    Applying promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,9 @@
 function getFullResponseFromAPI(boolean) {
-	return new Promise((resolve, reject) => {
-		if (boolean) {
-			resolve ({
-				status: 200,
-				body: 'Success'});
-		} else {
-			reject(new Error('The fake API is not working currently'));
-		}
-	});
+  return new Promise((resolve, reject) => {
+    if (boolean) {
+      resolve({ status: 200, body: 'Success' });
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
 }
